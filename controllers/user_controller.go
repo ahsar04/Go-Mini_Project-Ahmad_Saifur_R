0459208b031ca585b1b1db54e57e39e9578f4ef5
A Saifur R <asrsaif04@gmail.com>
2023-05-07 03:40:58 +0700

    add hash password to user

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"code_structure/config"
+	"code_structure/helpers"
 	"code_structure/middlewares"
 	"code_structure/models"
 	"net/http"
@@ -42,19 +43,28 @@ func GetUserController(c echo.Context) error {
 }
 // create new user
 func CreateUserController(c echo.Context) error {
-	user := models.User{}
-	c.Bind(&user)
+    user := models.User{}
+    c.Bind(&user)
 
+    // Hash password before saving to database
+    hashedPassword, err := helpers.HashPassword(user.Password)
+    if err != nil {
+        return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+    }
+    user.Password = string(hashedPassword)
 
-	if err := config.DB.Save(&user).Error; err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-	}
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"status": http.StatusOK,
-		"message": "success create new user",
-		"user":    user,
-	})
+    if err := config.DB.Save(&user).Error; err != nil {
+        return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+    }
+	userResponse := models.UserResponse{int(user.ID),user.Name,user.Email,user.Phone}
+
+    return c.JSON(http.StatusOK, map[string]interface{}{
+        "status":  http.StatusOK,
+        "message": "success create new user",
+        "user":    userResponse,
+    })
 }
+
 // delete user by id
 func DeleteUserController(c echo.Context) error {
 	user := models.User{}
@@ -97,26 +107,37 @@ func UpdateUserController(c echo.Context) error {
 	})
 }
 func LoginUserController(c echo.Context) error {
-	user := models.User{}
-	c.Bind(&user)
+    user := models.User{}
+    c.Bind(&user)
+	pass := user.Password
 
+    // Get user from database by email
+    if err := config.DB.Where("email = ?", user.Email).First(&user).Error; err != nil {
+        return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+            "message": "fail login",
+            "error":   err.Error(),
+        })
+    }
 
-	err := config.DB.Where("email = ? AND password = ?",user.Email, user.Password).First(&user).Error
-	if  err != nil {
-		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
-			"message" : "fail login",
-			"error" : err.Error(),
-		})
-	}
+    // Verify password using bcrypt
+    if err := helpers.CheckPasswordHash(pass, user.Password); err != nil {
+        return c.JSON(http.StatusUnauthorized, map[string]interface{}{
+            "message": "fail login",
+            "error":   "invalid email or password",
+        })
+    }
 
-	token, err := middlewares.CreateToken(int(user.ID), user.Name)
-	if err!=nil {
-		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
-			"message" : "fail login",
-			"error" : err.Error(),
-		})
-	}
-	userResponse := models.UserLoginResponse{int(user.ID), user.Name, user.Email, user.Phone, token}
+    // Generate JWT token
+    token, err := middlewares.CreateToken(int(user.ID), user.Name)
+    if err != nil {
+        return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+            "message": "fail login",
+            "error":   err.Error(),
+        })
+    }
+
+    // Return user data and JWT token
+    userResponse := models.UserLoginResponse{int(user.ID), user.Name, user.Email, user.Phone, token}
     return c.JSON(http.StatusOK, map[string]interface{}{
         "status":  http.StatusOK,
         "message": "success login",
